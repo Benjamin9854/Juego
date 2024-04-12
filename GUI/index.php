fdbe2bbc6901473f6780bbb084383c8dfc7aeb63
Benjamin Miranda <benjaminvicente@hotmail.cl>
2024-04-12 14:07:45 -0300

    Interfaz del menu

--- GUI/index.php
+++ GUI/index.php
@@ -1,21 +1,27 @@
 <?php
-    include("conexion.php");
-    include("funciones.php");
-    $id=1;
-    $jugador = obtenerJugador($conexion, $id);
-    print_r($jugador);//probando
+    include("../DataBase/conexion.php");
 ?>
 
 <!DOCTYPE html>
 <html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    id:<?php echo $jugador['id_jugador'];?> <br>
-    Nombre:<?php echo $jugador['nombre_jugador'];?> <br>
-    Dinero:<?php echo $jugador['dinero_jugador'];?> <br>
-</body>
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <script src="https://cdn.tailwindcss.com"></script>
+      <title>Granja</title>
+  </head>
+  <body class="bg-center bg-no-repeat bg-[url('Imagenes/menu.jpg')] bg-gray-400 bg-blend-multiply">
+  <p class="text-7xl text-gray-500 dark:text-cyan-400 text-center py-5">Bienvenido granjero</p>
+    <form class="max-w-sm mx-auto">
+      <div class="mb-5">
+        <label for="nombre" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tu nombre</label>
+        <input type="nombre" id="nombre" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Escribe tu nombre granjero" required />
+      </div>
+      <div class="mb-5">
+        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tu contraseña</label>
+        <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+      </div>
+      <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+    </form>
+  </body>
 </html>
