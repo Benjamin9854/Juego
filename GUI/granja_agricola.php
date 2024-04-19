7a248f0a32425792324784719cf616cd291753f0
Pablo <pablonikolazz@gmail.com>
2024-04-19 14:10:32 -0300

    u

--- GUI/granja_agricola.php
+++ GUI/granja_agricola.php
@@ -5,8 +5,44 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
     <title>Granja agricola</title>
+    <style>
+        #cuadrado {
+            width: 100px;
+            height: 100px;
+            background-color: red;
+            position: absolute;
+
+            top: 570px;
+            left: 1000px;
+        }
+    </style>
 </head>
 <body>
     <div class="absolute -z-9000 inset-0 bg-no-repeat bg-[url('Imagenes/G_agricola.png')] bg-cover"></div>
+    <div id="cuadrado"></div>
+
+<script>
+    
+    class Planta {
+            constructor() {
+                this.esta_regado = false;
+            }
+
+            regar() {
+                this.esta_regado = true;
+                document.getElementById('cuadrado').style.backgroundColor = 'green';
+                // Lógica adicional según sea necesario
+            }
+
+            // Otras funciones de la planta
+        }
+        const cuadrado = document.getElementById('cuadrado');
+        const miPlanta = new Planta();
+
+        cuadrado.addEventListener('click', () => {
+            // Acción cuando haces click en el cuadrado (por ejemplo, regar la planta)
+            miPlanta.regar();
+        });
+</script>
 </body>
 </html>
