a742e2d839b85c29a87accebc858c701bb6fa3ed
Benjamin9854 <benjaminvicente@hotmail.cl>
2024-04-19 13:50:13 -0300

    fondo 2

---
+++ GUI/granja_agricola.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Granja agricola</title>
+</head>
+<body>
+    <div class="absolute -z-9000 inset-0 bg-no-repeat bg-[url('Imagenes/G_agricola.png')] bg-cover"></div>
+</body>
+</html>
