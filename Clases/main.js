176e57554a255bd8033d1c97331841421d1c968f
Benjamin Miranda <benjaminvicente@hotmail.cl>
2024-05-10 12:36:20 -0300

    Cuadrados

---
+++ Clases/main.js
@@ -0,0 +1,11 @@
+const cuadrado1 = document.getElementById('cuadrado1');
+// const Planta = require('./Planta')
+// // Crear un arreglo de 30 objetos plantas
+// const arregloPlantas = [];
+// for (let i = 0; i < 30; i++) {
+//     arregloPlantas.push(new Planta());
+// }
+
+cuadrado1.addEventListener('click', () => {
+    document.getElementById('cuadrado1').style.backgroundColor = 'green';
+});
